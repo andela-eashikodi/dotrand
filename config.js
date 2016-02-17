e7eea295aa2d4bc29d40a00f6da2c1ecc55a5a49
Onye Ashikodi <ashikodiemeka@gmail.com>
2016-02-17 13:05:57 +0100

    edit prod url

--- config.js
+++ config.js
@@ -11,7 +11,7 @@ config = {
     // When running Ghost in the wild, use the production environment.
     // Configure your URL and mail settings here
     production: {
-        url: 'http://dotrand.com/blog',
+        url: 'http://www.dotrand.com/blog',
         fileStorage: false,
         mail: {
           transport: 'SMTP',
